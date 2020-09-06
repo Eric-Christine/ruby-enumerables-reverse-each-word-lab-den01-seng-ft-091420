6f6c05bbf2b35ec201670d76b86ce36afbccedc0
Eric Christine <ericjchristine@gmail.com>
2020-09-06 17:13:29 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,20 @@
+require 'pry'
+def reverse_each_word(string)
+  
+  x = string.split(/ /)
+  # ["Hello", "there,", "and", "how", "are", "you?"]
+  
+  # x is an array, now we can take each item from the array and reverse it.
+  
+  
+  count = 0 
+  y =[]
+  
+  while count < x.length  do 
+    word = x[count]
+    count += 1
+    y.unshift(word.reverse)
+    y
+  end
+ 
+end
